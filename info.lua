ec8401e4728fa0fe69ee9efcc8415073d530f16e
crazya02 <alekfleischer@yahoo.com>
2016-05-27 18:04:12 -0700

    Add info.lua

---
+++ info.lua
@@ -0,0 +1,12 @@
+t = ...  
+
+t.shortname = "Git Saver"
+t.longname = "Git Saver"
+t.author = "crazya02"
+t.version = "1.0"
+t.minimumved = "b6"
+t.description = [[
+Adds the ability to save your level in a git-friendly format, allowing for easier
+collaboration on levels.
+]]
+t.overrideinfo = false
